c7f72c536c7427bf43c51305a04d7e4d46b7b3ee
Charles <charles.malec@hotmail.fr>
2025-09-07 21:59:59 +0200

    chatgpt correction navlink and iframe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import TopBanner from './components/TopBanner';
 import Accueil from './components/Accueil';
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,6 +13,7 @@ function Contact() {
             <div className="w-full h-64 bg-gray-300 rounded-lg" id="map-placeholder">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.034019891884!2d4.401809477765459!3d50.83053377166713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c4b35e3cfb91%3A0xcc0c9f9df432c8fd!2sBd%20Louis%20Schmidt%20103%2C%201040%20Etterbeek!5e0!3m2!1sfr!2sbe!4v1757254026064!5m2!1sfr!2sbe"
+                title="Google Maps location"
                 width="100%"
                 height="100%"
                 allowFullScreen=""
