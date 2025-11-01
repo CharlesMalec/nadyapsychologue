1f74fe22a62f6489780d4ebd47682a8a2a179af3
CharlesMalec <charles.malec@hotmail.fr>
2025-11-01 16:41:55 +0100

    Modification footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-[var(--accent-color)] text-[var(--text-color)] py-4 text-center">
       <h2 className="text-2xl font-bold mb-4">Portez-vous bien !</h2>
-      <p>© 2025 Une cration Charly-web - Tous droits réservés</p>
+      <p>© 2025 Une création Charly-web - Tous droits réservés</p>
     </footer>
   );
 }
